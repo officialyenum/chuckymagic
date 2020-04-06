0f5f1acf91bd71e447cda2fbc94fdcb64434a943
Opone Chukwuyenum <oponechukwuyenum@icloud.com>
2020-04-06 21:17:28 +0100

    setup of express js server

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+console.log('hello')
+const express = require('express')
+const bodyparser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('combined'))
+app.use(bodyparser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => { res.send({ message: ' hello!' }) })
+
+app.listen(process.env.PORT || 8081)
