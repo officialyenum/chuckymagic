29e224d99a9edfa8d4744844a2b2edf885dfc860
Opone Chukwuyenum <oponechukwuyenum@icloud.com>
2020-04-08 20:36:54 +0100

    setting up registration page

--- server/src/app.js
+++ server/src/app.js
@@ -10,6 +10,6 @@ app.use(morgan('combined'))
 app.use(bodyparser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => { res.send({ message: ' hello!' }) })
+app.post('/register', (req, res) => { res.send({ message: `Hello ${req.body.email}! Your user was registered! Have fun!` }) })
 
 app.listen(process.env.PORT || 8081)
