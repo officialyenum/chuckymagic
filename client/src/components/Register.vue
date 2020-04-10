502611730dfeffbb30235ba67b86fcd8c466060f
Opone Chukwuyenum <oponechukwuyenum@icloud.com>
2020-04-10 14:25:48 +0100

    register authentication working commit

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    <h1>Register</h1>
+    <h1 class="header">Register</h1>
+    <input type="username" name="username" v-model="username" placeholder="username" />
+    <br>
     <input type="email" name="email" v-model="email" placeholder="email" />
     <br>
     <input type="password" name="password" v-model="password" placeholder="password" />
     <br>
+    <div class="error" v-html="error"/>
     <button type="submit" @click="register">Register</button>
   </div>
 </template>
@@ -14,17 +17,23 @@ import AuthenticationService from '@/services/AuthenticationService'
 export default {
   data () {
     return {
+      username: '',
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          username: this.username,
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -32,4 +41,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.error {
+  padding: 20px;
+  font-family: Georgia, 'Times New Roman', Times, serif;
+  font-size: 10px;
+  color: red;
+  size: 5px;
+}
+.header {
+  text-align: center;
+  font: 100;
+  font-family:fantasy;
+  font-size: 32px;
+  color: aqua;
+}
 </style>
